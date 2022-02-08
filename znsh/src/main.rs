baa53d15be04b74ec4ed83625970f921e8ee10a9
zjnett <zjnett@mac.com>
2022-02-07 23:31:17 -0800

    Add basic command prompt

--- znsh/src/main.rs
+++ znsh/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    println!("************* Welcome to znsh **************");
+    // Infinite loop until exit is called
+    loop {
+        print!("user@znsh:/insert/cwd/here ➜ ");
+        // Take user input and write to a string line
+        io::stdout().flush().unwrap();
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        print!("{}", line);
+    }
 }
