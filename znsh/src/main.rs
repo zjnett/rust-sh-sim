38a35a18cc5df21fbb5a39872b32723c57f7e312
zjnett <zjnett@mac.com>
2022-02-08 00:15:34 -0800

    Add cd and exit, truncate newline from cmd

--- znsh/src/main.rs
+++ znsh/src/main.rs
@@ -1,7 +1,11 @@
 use std::io;
 use std::io::Write;
+use std::env;
+use std::path::Path;
 
 fn main() {
+    let home = dirs::home_dir().unwrap();
+
     println!("************* Welcome to znsh **************");
     // Infinite loop until exit is called
     loop {
@@ -10,6 +14,37 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).unwrap();
-        print!("{}", line);
+        // Convert line to lowercase for processing
+        line = line.to_lowercase();
+        // Truncate newline from line
+        line.truncate(line.len() - 1);
+        println!("line = {}", line);
+
+        // Tokenize line into substrings, collect iterator into a vector names
+        let names: Vec<&str> = line.split(" ").collect();
+        println!("{}", names[0]);
+
+        // Implement cd wrapper command
+        if names[0] == "cd" {
+            if names.len() > 1 {
+                // names[1] exists
+                let path = Path::new(names[1]);
+                // Check if path exists
+                if path.exists() {
+                    env::set_current_dir(path)
+                        .expect(format!("cd: could not chdir() to path {}", names[1]).as_str());
+                } else {
+                    println!("cd: no such file or directory: {}", names[1])
+                }
+            }
+            // otherwise, chdir to HOME
+            env::set_current_dir(home.as_path())
+                .expect("Error: could not chdir() to HOME");
+        }
+
+        // Implement exit wrapper command
+        if names[0] == "exit" {
+            std::process::exit(0);
+        }
     }
 }
